07c55d7942f1d24a2d969b237de1a42c3b072d4c
DLow148 <dereklow148@hotmail.com>
2017-05-09 10:16:33 -0400

    Added a GUI, Bezier in progress

--- imagemapping.py
+++ imagemapping.py
@@ -1,16 +1,10 @@
+#Edited by Derek Low to apply in GUI format
 import numpy as np
 import cv2
-import Tkinter as tk
-import tkFileDialog
-
-'''root = tk.Tk()
-root.withdraw()
-file_path = tkFileDialog.askopenfilename()
-
-srcimg = cv2.imread(file_path)
-cv2.imshow('source image', srcimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()'''
+import os, sys
+from Tkinter import *
+from tkFileDialog import askopenfilename
+from PIL import Image, ImageTk
 
 # Read points from text file
 def readPoints(path) :
@@ -79,18 +73,131 @@ def morphTriangle(img1, img2, img, t1, t2, t, alpha) :
     warpImage2 = applyAffineTransform(img2Rect, t2Rect, tRect, size)
 
     # Alpha blend rectangular patches
-    imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2
+    imgRect = alpha * warpImage2
+    #imgRect = (1.0 - alpha) * warpImage1 + alpha * warpImage2
 
     # Copy triangular region of the rectangular patch to the output image
     img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] = img[r[1]:r[1]+r[3], r[0]:r[0]+r[2]] * ( 1 - mask ) + imgRect * mask
 
+#on Button Press, open a file and record that pixel, and add a green dot to the button press
+def coordsToText(event):
+    python_hotPink = "#FF69B4"
+    x1, y1 = (event.x-3), (event.y -3)
+    x2, y2 = (event.x+3), (event.y +3)
+    canvas.create_oval(x1,y1,x2,y2, fill = python_green)
+    f = open(File + ".txt","a")
+    f.write(str(event.x) + " " +str(event.y)+"\n")
+    f.close()
+
+#helper function so we can bind to a button on the interface
+def coordsToTextHelper():
+    canvas.bind("<Button 1>", coordsToText)
+    resetButton = Button(root, text = "Reset", command = textResetter)
+    resetButton.pack()
+
+#reset both the associated text file and the canvas to set new points
+def textResetter():
+    os.remove(File+".txt")
+    canvas.delete("all")
+    canvas.create_image(0,0,image=img,anchor="nw")
+
+'''def zoomIn():
+    image1 = Image.open(File)
+    image1 = image1.resize((imgx * 5, imgy * 5), Image.ANTIALIAS) #The (250, 250) is (height, width)
+    imgResize = ImageTk.PhotoImage(image1)
+    canvas.create_image(0,0,image = imgResize, anchor = "nw")'''
+
+#Here's where we do all the Bezier curve stuff. Start off by generating the global array that will be used
+xys = [] #this is where the xys values will be stored for the bezier curve production
+
+#This portion is the user-end portion that allows the user to select points and view the points they've selected
+def pointSelectBezier(event):
+    python_yellow = "#FFD700"
+    x1, y1 = (event.x -3), (event.y -3)
+    x2, y2 = (event.x +3), (event.y +3)
+    canvas.create_oval(x1,y1,x2,y2, fill = python_yellow)
+    xys.append((event.x,event.y))
+
+#Functions for calculating the points of Bezier curves that I found on the internet at
+#http://stackoverflow.com/questions/246525/how-can-i-draw-a-bezier-curve-using-pythons-pil
+def make_bezier(xys):
+    # xys should be a sequence of 2-tuples (Bezier control points)
+    n = len(xys)
+    combinations = pascal_row(n-1)
+    def bezier(ts):
+        # This uses the generalized formula for bezier curves
+        # http://en.wikipedia.org/wiki/B%C3%A9zier_curve#Generalization
+        result = []
+        for t in ts:
+            tpowers = (t**i for i in range(n))
+            upowers = reversed([(1-t)**i for i in range(n)])
+            coefs = [c*a*b for c, a, b in zip(combinations, tpowers, upowers)]
+            result.append(
+                tuple(sum([coef*p for coef, p in zip(coefs, ps)]) for ps in zip(*xys)))
+        return result
+    return bezier
+
+def pascal_row(n):
+    # This returns the nth row of Pascal's Triangle
+    result = [1]
+    x, numerator = 1, n
+    for denominator in range(1, n//2+1):
+        # print(numerator,denominator,x)
+        x *= numerator
+        x /= denominator
+        result.append(x)
+        numerator -= 1
+    if n&1 == 0:
+        # n is even
+        result.extend(reversed(result[:-1]))
+    else:
+        result.extend(reversed(result))
+    return result
+
+#quit window to move on to the Triangulation
+def exitWindow():
+    root.destroy()
 
 if __name__ == '__main__' :
+    root = Tk()
 
+    #adding the image, then create frame and canvas based on image size
+    File = askopenfilename(parent = root, initialdir="C:/users/Derek/Desktop/WORMS PROJECT",title = 'Choose an image')
+    img=ImageTk.PhotoImage(Image.open(File))
+    root.img = img
+    imgx = img.width()
+    imgy = img.height()
+
+    #Set up Tkinter canvas and frame based on size of image
+    frame = Frame(root, bd=2, relief=SUNKEN)
+    canvas = Canvas(frame, bd=0,width = imgx, height=imgy)
+    frame.pack()
+    canvas.pack()
+    canvas.create_image(0,0,image=img,anchor="nw")
+
+#INITIAL GUI BUTTONS
+    #This buton will allow you to select points and add them to a text file, and will also give you a reset button to
+    #reset the points if need be
+    pointButton = Button(root,text = "Set Points for Triangulation!", command = coordsToTextHelper)
+    pointButton.pack()
+
+    #This button will allow you to place points for Bezier Curve selection
+
+
+    #This button will close the window, proceeding with the triangulation based on the files and points selected
+    exitButton = Button(root,text = "Run Triangulation!", command = exitWindow)
+    exitButton.pack()
+
+    #Run the main point-selection loop
+    root.mainloop()
+
+#RUN THE IMAGE MORPHING
     filename1 = 'flatwormEX1.jpg'
-    filename2 = 'flatwormEX2.jpg'
-    alpha = 0.9
-
+    filename2 = File
+    #By setting coordAlpha to 0 and pixelAlpha to 1, you use actual pixel colors of file 2
+    #and fit it to the shape of file 1
+    pixelAlpha = 1.0
+    coordAlpha = 0.0
     # Read images
     img1 = cv2.imread(filename1);
     img2 = cv2.imread(filename2);
@@ -106,8 +213,10 @@ if __name__ == '__main__' :
 
     # Compute weighted average point coordinates
     for i in xrange(0, len(points1)):
-        x = ( 1 - alpha ) * points1[i][0] + alpha * points2[i][0]
-        y = ( 1 - alpha ) * points1[i][1] + alpha * points2[i][1]
+        x = ( 1 - coordAlpha ) * points1[i][0] + coordAlpha * points2[i][0]
+        y = ( 1 - coordAlpha ) * points1[i][1] + coordAlpha * points2[i][1]
+        #x = ( 1 - alpha ) * points1[i][0] + alpha * points2[i][0]
+        #y = ( 1 - alpha ) * points1[i][1] + alpha * points2[i][1]
         points.append((x,y))
 
     print (points)
@@ -129,9 +238,9 @@ if __name__ == '__main__' :
             t = [ points[x], points[y], points[z] ]
 
             # Morph one triangle at a time.
-            morphTriangle(img1, img2, imgMorph, t1, t2, t, alpha)
+            morphTriangle(img1, img2, imgMorph, t1, t2, t, pixelAlpha)
 
 
     # Display Result
-    cv2.imshow("Morphed Face", np.uint8(imgMorph))
+    cv2.imshow("Morphed Worm", np.uint8(imgMorph))
     cv2.waitKey(0)
